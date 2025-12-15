8906d4596c335aa0c595f122dd692e121604656a
Arik Chakma <arikchangma@gmail.com>
2025-12-15 17:57:30 +0600

    wip

--- src/crypto.test.ts
+++ src/crypto.test.ts
@@ -5,20 +5,20 @@ const secret = '1234567890';
 const data = { name: 'John Doe', email: 'john.doe@example.com' };
 
 describe('crypto', () => {
-  it('should encrypt and decrypt data', async () => {
-    const encrypted = await encrypt(secret, data);
-    const decrypted = await decrypt(secret, encrypted);
+  it('should encrypt and decrypt data', () => {
+    const encrypted = encrypt(secret, data);
+    const decrypted = decrypt(secret, encrypted);
     expect(decrypted).toEqual(data);
   });
-  it('should return "false" if the data is invalid', async () => {
-    const encrypted = await encrypt(secret, data);
-    const decrypted = await decrypt(secret, encrypted + 'invalid');
+  it('should return "false" if the data is invalid', () => {
+    const encrypted = encrypt(secret, data);
+    const decrypted = decrypt(secret, encrypted + 'invalid');
     expect(decrypted).toBe(false);
     expect(decrypted).toBeFalsy();
   });
-  it('should return "false" if the secret is invalid', async () => {
-    const encrypted = await encrypt(secret, data);
-    const decrypted = await decrypt(secret + 'invalid', encrypted);
+  it('should return "false" if the secret is invalid', () => {
+    const encrypted = encrypt(secret, data);
+    const decrypted = decrypt(secret + 'invalid', encrypted);
     expect(decrypted).toBe(false);
     expect(decrypted).toBeFalsy();
   });
---
+++ src/utils.test.ts
@@ -0,0 +1,39 @@
+import { describe, expect, it } from 'bun:test';
+import * as oauth from 'oauth4webapi';
+import { buildAuthorizationUrl } from './utils';
+
+describe('utils', () => {
+  it('should build authorization url', async () => {
+    const codeVerifier = oauth.generateRandomCodeVerifier();
+    const url = await buildAuthorizationUrl({
+      authorizationEndpoint: 'https://example.com/authorize',
+      clientId: '1234567890',
+      redirectUri: 'https://example.com/callback',
+      scopes: ['openid', 'profile', 'email'],
+      codeVerifier,
+      state: '1234567890',
+      extraParams: {
+        access_type: 'offline',
+        prompt: 'consent',
+        include_granted_scopes: 'true',
+      },
+    });
+
+    const searchParams = new URL(url).searchParams;
+    expect(searchParams.get('client_id')).toBe('1234567890');
+    expect(searchParams.get('redirect_uri')).toBe(
+      'https://example.com/callback'
+    );
+    expect(searchParams.get('scope')).toBe('openid profile email');
+
+    const codeChallenge = await oauth.calculatePKCECodeChallenge(codeVerifier);
+    expect(searchParams.get('code_challenge')).toBe(codeChallenge);
+    expect(searchParams.get('code_challenge_method')).toBe('S256');
+
+    expect(searchParams.get('state')).toBe('1234567890');
+    expect(searchParams.get('response_type')).toBe('code');
+    expect(searchParams.get('access_type')).toBe('offline');
+    expect(searchParams.get('prompt')).toBe('consent');
+    expect(searchParams.get('include_granted_scopes')).toBe('true');
+  });
+});
